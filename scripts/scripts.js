6f97d0266d053c1813f9c200a87fc802aa88fe6c
Jeff Rudd <jeffrudd19@gmail.com>
2019-08-27 18:31:26 -0400

    Initial commit

---
+++ scripts/scripts.js
@@ -0,0 +1,30 @@
+// Create app namespace to hold all methods
+const ronSwansonApp = {};
+
+// Collect user input based on user selection
+ronSwansonApp.userChoice = function () {
+    
+}
+
+// Make AJAX request with user inputted data
+ronSwansonApp.getInfo = function () {
+
+}
+
+// Filter array based on user inputted data
+
+
+// Display data on the page
+ronSwansonApp.displayInfo = function () {
+
+}
+
+// Start ronSwansonApp
+ronSwansonApp.init = function () {
+
+}
+
+// Once the page is loaded run all functions in ronSwansonApp
+$(document).ready(function () {
+    ronSwansonApp.init();
+});
