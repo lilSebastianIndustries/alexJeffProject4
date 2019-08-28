5ad03bb3782475f8c001c736202e5a03db80ea72
Jeff Rudd <jeffrudd19@gmail.com>
2019-08-28 19:20:36 -0400

    created filtered array

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,18 +1,45 @@
 // Create app namespace to hold all methods
 const ronSwansonApp = {};
 
+const apiUrl = "https://ron-swanson-quotes.herokuapp.com/v2/quotes/63";
+
+
 // Collect user input based on user selection
 ronSwansonApp.userChoice = function () {
     
 }
 
 // Make AJAX request with user inputted data
-ronSwansonApp.getInfo = function () {
+ronSwansonApp.getQuotes = function () {
 
+    $.ajax({
+        url: apiUrl,
+        method: 'GET',
+        dataType: 'json'
+    }).then(function(results) {
+        console.log(results);
+        ronSwansonApp.filterString = () => {
+            const foodArray = []
+            results.forEach( string => {
+                if (string.toLowerCase().includes("fish") || string.toLowerCase().includes("food") === true) {
+                    return foodArray.push(string)
+                    
+                }
+                
+            })
+            console.log(foodArray)
+            
+        }
+        ronSwansonApp.filterString();
+    }).fail((error) => {
+        console.log(error);
+    });
 }
 
-// Filter array based on user inputted data
+    ronSwansonApp.getQuotes();
+    
 
+// Filter array based on user inputted data
 
 // Display data on the page
 ronSwansonApp.displayInfo = function () {
